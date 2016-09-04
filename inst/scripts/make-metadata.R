d12b0865a6ab3eab848d037ccf6e0df11cd5edbb
Lucas Schiffer <schifferl@users.noreply.github.com>
2016-09-04 12:47:31 -0400

    addition of metadata and upload scripts

--- inst/scripts/make-metadata.R
+++ inst/scripts/make-metadata.R
@@ -1,3 +1,22 @@
 make_metadata <- function() {
-
+    Title <- ""
+    Description <- ""
+    BiocVersion <- ""
+    Genome <- ""
+    SourceType <- ""
+    SourceUrl <- ""
+    SourceVersion <- ""
+    Species <- ""
+    TaxonomyId <- ""
+    Coordinate_1_based <- ""
+    DataProvider <- ""
+    Maintainer <- ""
+    RDataClass <- ""
+    DispatchClass <- ""
+    ResourceName <- ""
+    metadata <- data.frame(Title, Description, BiocVersion, Genome, SourceType,
+                           SourceUrl, SourceVersion, Species, TaxonomyId,
+                           Coordinate_1_based, DataProvider, Maintainer,
+                           RDataClass, DispatchClass, ResourceName)
+    write_csv(metadata, append = TRUE)
 }
