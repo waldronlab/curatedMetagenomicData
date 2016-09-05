171ef8853ed09c49e579eec4c12a3756aefa6bf5
Lucas Schiffer <schifferl@users.noreply.github.com>
2016-09-05 11:56:12 -0400

    addition of na_zero and make_upload functions

--- inst/scripts/pathcoverage.R
+++ inst/scripts/pathcoverage.R
@@ -4,5 +4,6 @@ pathcoverage <- function(pheno_data, dataset_dir) {
     assay_merge() %>%
     fix_rownames() %>%
     fix_colnames(., data_type) %>%
+    na_zero() %>%
     bodysite_eset(., pheno_data, dataset_dir, data_type)
 }
