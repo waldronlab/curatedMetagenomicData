9cc0c0cbcb777dfa870a7c99a0d70a1c3be73932
“Lucas <schifferl@users.noreply.github.com>
2016-09-08 14:05:35 -0400

    working parallel_join function

--- inst/scripts/parallel_join.R
+++ inst/scripts/parallel_join.R
@@ -4,11 +4,20 @@ parallel_join <- function(assay_list) {
         even_assays <- assay_list[seq.int(2, assay_length, 2)]
         if(assay_length %% 2) {
             odds_assays <- assay_list[seq.int(1, assay_length - 1, 2)]
-            last_assay <- assay_list[[assay_length]]
-            bplapply(odds_assays, join_assays, even_assays)
+            last_assay <- assay_list[assay_length]
+            assay_list <- bpmapply(join_assays, odds_assays, even_assays,
+                                   SIMPLIFY = FALSE)
         } else {
             odds_assays <- assay_list[seq.int(1, assay_length, 2)]
-            bplapply(odds_assays, join_assays, even_assays)
+            assay_list <- bpmapply(join_assays, odds_assays, even_assays,
+                                   SIMPLIFY = FALSE)
         }
+    }
+    if(exists("last_assay")) {
+        first_assay <- assay_list[[1]]
+        second_assay <- last_assay[[1]]
+        join_assays(first_assay, second_assay)
+    } else {
+        assay_list[[1]]
     }
 }
