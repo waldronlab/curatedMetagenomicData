d836f196716a25a0f00db7f2af336fd016666328
“Lucas <schifferl@users.noreply.github.com>
2016-09-26 17:08:53 -0400

    correction to parallel functions

--- inst/scripts/parallel_join.R
+++ inst/scripts/parallel_join.R
@@ -7,17 +7,12 @@ parallel_join <- function(assay_list) {
             last_assay <- assay_list[assay_length]
             assay_list <- bpmapply(join_assays, odds_assays, even_assays,
                                    SIMPLIFY = FALSE)
+            assay_list <- c(assay_list, last_assay)
         } else {
             odds_assays <- assay_list[seq.int(1, assay_length, 2)]
             assay_list <- bpmapply(join_assays, odds_assays, even_assays,
                                    SIMPLIFY = FALSE)
         }
     }
-    if(exists("last_assay")) {
-        first_assay <- assay_list[[1]]
-        second_assay <- last_assay[[1]]
-        join_assays(first_assay, second_assay)
-    } else {
-        assay_list[[1]]
-    }
+    assay_list[[1]]
 }
--- inst/scripts/parallel_rbind.R
+++ inst/scripts/parallel_rbind.R
@@ -7,17 +7,12 @@ parallel_rbind <- function(metadata_list) {
             last_element <- metadata_list[metadata_length]
             metadata_list <- bpmapply(bind_rows, odds_elements, even_elements,
                                    SIMPLIFY = FALSE)
+            metadata_list <- c(metadata_list, last_element)
         } else {
             odds_elements <- metadata_list[seq.int(1, metadata_length, 2)]
             metadata_list <- bpmapply(bind_rows, odds_elements, even_elements,
                                    SIMPLIFY = FALSE)
         }
     }
-    if(exists("last_element")) {
-        first_element <- metadata_list[[1]]
-        second_element <- last_element[[1]]
-        bind_rows(first_element, second_element)
-    } else {
-        metadata_list[[1]]
-    }
+    metadata_list[[1]]
 }
