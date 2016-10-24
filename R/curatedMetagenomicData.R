ee1eafe6f1a74e4be13b68bfb78138450da6cd95
schifferl <schifferl@users.noreply.github.com>
2016-10-24 11:38:21 -0400

    documentation and function changes
    Former-commit-id: ece84642a3522753158f3358d6f099838042d02a

--- R/curatedMetagenomicData.R
+++ R/curatedMetagenomicData.R
@@ -1,7 +1,10 @@
 #' Curated Metagenomic Data of the Human Microbiome
 #'
 #' The curatedMetagenomicData package provides taxonomic, functional, and gene
-#' marker abundance for samples collected from different bodysites.
+#' marker abundance for samples collected from different bodysites. It provides
+#' data from aproximately 3000 human microbiome samples that has been highly
+#' processed, refined, and curated such that analysis that might otherwise
+#' require a computing cluster can be done on an ordianary laptop.
 #'
 #' @name curatedMetagenomicData
-NULL
+"_PACKAGE"
--- inst/scripts/pubmed_query.R
+++ inst/scripts/pubmed_query.R
@@ -1,5 +1,7 @@
 pubmed_query <- function(metadata) {
     select(metadata, pubmedid) %>%
+    unlist() %>%
+    as.integer() %>%
     unique() %>%
     pubmed_check() %>%
     EUtilsGet()
