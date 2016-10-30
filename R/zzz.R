51d3c2164f3c216863617cba85ef3fc04e7e1ca2
Lucas Schiffer <schifferl@users.noreply.github.com>
2016-10-30 14:30:50 -0400

    changes to be tested with travis ci
    Former-commit-id: 9ce7a42c447310794a121de21e56ec106083fb49

--- R/zzz.R
+++ R/zzz.R
@@ -1,35 +1,32 @@
 #' @keywords internal
 #' @importFrom utils read.csv
 #' @importFrom AnnotationHub query
+#' @importFrom ExperimentHub ExperimentHub
 .onLoad <- function(libname, pkgname) {
-    titles <- read.csv(
-        system.file("extdata", "metadata.csv",
-                    package = "curatedMetagenomicData"),
-        stringsAsFactors = FALSE
-    )$Title
-    rda <- gsub(".rda", "", titles, fixed = TRUE)
+    titles <- read.csv(system.file("extdata", "metadata.csv",
+                                   package="curatedMetagenomicData"),
+                       stringsAsFactors=FALSE)$Title
+    rda <- gsub(".rda", "", titles, fixed=TRUE)
     if (!length(rda))
         stop("no .rda objects found in metadata")
 
     ## Functions to load resources by name:
-    ns <- asNamespace("curatedMetagenomicData")
+    ns <- asNamespace(pkgname)
     sapply(rda,
            function(xx) {
                func = function(metadata = FALSE) {
                    if (!isNamespaceLoaded("ExperimentHub"))
                        attachNamespace("ExperimentHub")
-                   eh <-
-                       AnnotationHub::query(ExperimentHub(), "curatedMetagenomicData")
+                   eh <- query(ExperimentHub(), "curatedMetagenomicData")
                    ehid <- names(query(eh, xx))
                    if (!length(ehid))
                        stop(paste0("resource ", xx,
                                    "not found in ExperimentHub"))
                    if (metadata)
                        eh[ehid]
-                   else
-                       eh[[ehid]]
+                   else eh[[ehid]]
                }
-               assign(xx, func, envir = ns)
+               assign(xx, func, envir=ns)
                namespaceExport(ns, xx)
            })
 }
