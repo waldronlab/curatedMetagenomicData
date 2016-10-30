9ce7a42c447310794a121de21e56ec106083fb49
Lucas Schiffer <schifferl@users.noreply.github.com>
2016-10-30 14:30:50 -0400

    changes to be tested with travis ci

--- tests/testthat/test-ExpressionSet2phyloseq.R
+++ tests/testthat/test-ExpressionSet2phyloseq.R
@@ -1,6 +1,7 @@
+context("ExpressionSet2phyloseq")
+
 test_that("ExpressionSet2phyloseq returns phyloseq class object", {
-    LomanNJ_2013_Mi.metaphlan_bugs_list.stool() %>%
-    ExpressionSet2phyloseq() %>%
-    class() %>%
-    expect_equal("phyloseq")
+    test_object <- LomanNJ_2013_Mi.metaphlan_bugs_list.stool() %>%
+    ExpressionSet2phyloseq()
+    expect_is(test_object, "phyloseq")
 })
