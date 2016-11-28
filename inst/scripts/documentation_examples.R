07c30474ada3248875fe295a58b39289e5913477
schifferl <schifferl@users.noreply.github.com>
2016-11-28 11:17:15 -0500

    ensuring 4 spaces for indentation
    Former-commit-id: 2cb804ebaa32db92fa46f8e4ac42a526d8533592

--- inst/scripts/documentation_examples.R
+++ inst/scripts/documentation_examples.R
@@ -1,4 +1,4 @@
 documentation_examples <- function(first_element, documentation_df) {
-  documentation_df$aliases[first_element] %>%
-  paste0("\n#' @examples ", ., "()\n#'")
+    documentation_df$aliases[first_element] %>%
+    paste0("\n#' @examples ", ., "()\n#'")
 }
