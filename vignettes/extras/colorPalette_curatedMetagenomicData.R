957cc0828103d5543d92d54048c368aeb9b9637f
audreyrenson <audrey.o.renson@gmail.com>
2016-12-19 14:26:52 -0500

    Update colorPalette_curatedMetagenomicData.R

--- vignettes/extras/colorPalette_curatedMetagenomicData.R
+++ vignettes/extras/colorPalette_curatedMetagenomicData.R
@@ -1,7 +1,3 @@
-#################################################
-#  Color-blind friendly universal color palette #
-#  for curatedMetagenomicData figures.          #
-#################################################
 blue = "#3366aa"
 blueGreen = "#11aa99"
 green = "#66aa55"
@@ -12,4 +8,3 @@ red = "#ee3333"
 orange = "#ee7722"
 yellow = "#ffee33"
 pallet = c(blue, blueGreen, green, paleYellow, gray, purple, red, orange, yellow)
-
