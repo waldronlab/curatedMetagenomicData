9dae2af2df8a3fff8eb1eeb13f7261df252ace04
schifferl <schifferl@users.noreply.github.com>
2017-02-02 14:39:37 -0500

    correct col types when reading in metadata
    Former-commit-id: 4c359042eb408a6e1e8627a8e22a54c953501756

--- inst/scripts/metadata.R
+++ inst/scripts/metadata.R
@@ -1,5 +1,5 @@
 metadata <- function(dataset_dir) {
     metadata_tsv <- paste0(dataset_dir, "metadata/metadata.tsv")
-    guess_cols(metadata_tsv) %>%
+    metadata_cols(metadata_tsv) %>%
     read_tsv(metadata_tsv, col_types = ., na = c("", "NA", "na", "nA", "Na"))
 }
