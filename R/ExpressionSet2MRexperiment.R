997c331709761d397d8e9c82c266c300b80c3745
schifferl <schifferl@users.noreply.github.com>
2017-04-04 15:52:23 -0400

    corrected idententation

--- R/ExpressionSet2MRexperiment.R
+++ R/ExpressionSet2MRexperiment.R
@@ -6,7 +6,7 @@
 #'
 #' @param simplify
 #'
-#' if TRUE the most detailed clade name is used, instead of the original 
+#' if TRUE the most detailed clade name is used, instead of the original
 #' metaPhlAn2 names which contain the full taxonomy.
 #'
 #' @return
@@ -25,29 +25,25 @@
 #' @importFrom magrittr %>%
 #' @importFrom dplyr data_frame
 #' @importFrom tidyr separate
-ExpressionSet2MRexperiment <- function(eset, simplify = TRUE){
-  library(magrittr)
-  library(tidyr)
-  library(metagenomeSeq)
-  taxonomic.ranks <- c("Kingdom", "Phylum", "Class", "Order", "Family",
-                       "Genus", "Species", "Strain")
-  otu.table <- exprs(eset)
-  if(max(otu.table) < 101) {
-    otu.table <- round(sweep(otu.table, 2, eset$number_reads/100, "*"))
-  }
-  tax.table <- rownames(otu.table) %>%
-    gsub("[a-z]__", "", .) %>%
-    data_frame() %>%
-    separate(., ".", taxonomic.ranks, sep = "\\|", fill="right") %>%
-    as.matrix()
-  if(simplify) {
-    rownames(otu.table) <- rownames(otu.table) %>%
-      gsub(".+\\|", "", .)
-  }
-  rownames(tax.table) <- rownames(otu.table)
-  tax.table <- AnnotatedDataFrame(data.frame(tax.table))
-  mgseq = newMRexperiment(counts = otu.table,
-                          phenoData = phenoData(eset),
-                          featureData = tax.table)
-  return(mgseq)
+ExpressionSet2MRexperiment <- function(eset, simplify = TRUE) {
+    taxonomic.ranks <- c("Kingdom", "Phylum", "Class", "Order", "Family",
+                         "Genus", "Species", "Strain")
+    otu.table <- exprs(eset)
+    if(max(otu.table) < 101) {
+        otu.table <- round(sweep(otu.table, 2, eset$number_reads/100, "*"))
+    }
+    tax.table <- rownames(otu.table) %>%
+        gsub("[a-z]__", "", .) %>%
+        data_frame() %>%
+        separate(., ".", taxonomic.ranks, sep = "\\|", fill = "right") %>%
+        as.matrix()
+    if(simplify) {
+        rownames(otu.table) <- rownames(otu.table) %>%
+            gsub(".+\\|", "", .)
+    }
+    rownames(tax.table) <- rownames(otu.table)
+    tax.table <- AnnotatedDataFrame(data.frame(tax.table))
+    mgseq = newMRexperiment(counts = otu.table, phenoData = phenoData(eset),
+                            featureData = tax.table)
+    return(mgseq)
 }
