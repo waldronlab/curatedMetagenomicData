e86897286cf588b3c278515266a61f661a11a533
schifferl <schifferl@users.noreply.github.com>
2017-04-12 18:07:52 -0400

    incremental changes to documentation functions

--- data-raw/documentation_source.R
+++ data-raw/documentation_source.R
@@ -1,4 +1,3 @@
 documentation_source <- function(first_element, documentation_df) {
-    documentation_df$source[first_element] %>%
-    paste0("\n#' @source ", ., "\n#'")
+
 }
---
+++ data-raw/make_data_figures.R
@@ -0,0 +1,3 @@
+make_data_figures <- function() {
+
+}
