0f1acc4c9ed8197dc4aa9f1df8ef698ce96f2ed7
Lucas Schiffer <schifferl@users.noreply.github.com>
2017-04-23 12:52:45 -0400

    functions to produce documentation with figures and an example thereof
    Former-commit-id: 1fdfe80904973482d9d00faebb7275107e35a2f2

--- data-raw/documentation_eda.R
+++ data-raw/documentation_eda.R
@@ -1,4 +1,7 @@
 documentation_eda <- function(first_element, documentation_df) {
     dataset_name <- documentation_df$seealso[first_element]
-    figure_name <- dir("./man/figures")
+    figure_names <- dir("./man/figures")
+    figure_subset <- startsWith(figure_names, dataset_name)
+    figure_names[figure_subset] %>%
+        paste0("\n#' \\figure{", ., "}{options: width=\"100\\%\"}\n#'", collapse = "")
 }
--- data-raw/get_experiment_data.R
+++ data-raw/get_experiment_data.R
@@ -1,6 +1,6 @@
 get_experiment_data <- function(resource_object, slot_name) {
     experimentData(resource_object) %>%
         slot(., slot_name) %>% {
-            ifelse(length(.) > 1L, ., NA)
+            ifelse(nchar(.) > 1, ., NA)
         }
 }
