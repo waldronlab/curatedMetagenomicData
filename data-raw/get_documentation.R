a18f7152cbc054aa07b3fc2f3dc2e9140b1cdc2f
schifferl <schifferl@users.noreply.github.com>
2017-06-05 18:56:37 -0400

    changes related to addition of version datestamp

--- data-raw/get_documentation.R
+++ data-raw/get_documentation.R
@@ -1,12 +1,13 @@
 get_documentation <- function(resource_name) {
+    browser()
     resource_object <- get_resource(resource_name)
     title <- get_title(resource_name)
     aliases <- gsub(".rda", "", resource_name)
     subsection <- get_subsection(resource_object)
-    publication <- get_experiment_data(resource_object, "title")
+    publication <- get_experiment_data(resource_object, "title") %>% paste0(collapse = "; ")
     authors <- get_experiment_data(resource_object, "name")
     affiliations <- get_experiment_data(resource_object, "lab")
-    pmid <- get_experiment_data(resource_object, "pubMedIds")
+    pmid <- get_experiment_data(resource_object, "pubMedIds") %>% paste0(collapse = "; ")
     technology <- get_technology(resource_object)
     name <- get_name(resource_name)
     seealso <- strsplit(resource_name, "\\.")[[1]][1]
--- tests/testthat/test-ExpressionSet2phyloseq.R
+++ tests/testthat/test-ExpressionSet2phyloseq.R
@@ -1,7 +1,9 @@
 context("ExpressionSet2phyloseq")
 
 test_that("ExpressionSet2phyloseq returns phyloseq class object", {
-    test_object <- LomanNJ_2013_Mi.metaphlan_bugs_list.stool() %>%
-    ExpressionSet2phyloseq()
+    test_object <-
+        LomanNJ_2013_Mi.metaphlan_bugs_list.stool() %>%
+        ExpressionSet2phyloseq()
+
     expect_is(test_object, "phyloseq")
 })
