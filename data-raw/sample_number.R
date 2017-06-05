0675a447af5ae943809955ebc5838f301ddb80e8
schifferl <schifferl@users.noreply.github.com>
2017-06-05 18:56:37 -0400

    changes related to addition of version datestamp
    Former-commit-id: a18f7152cbc054aa07b3fc2f3dc2e9140b1cdc2f

--- data-raw/sample_number.R
+++ data-raw/sample_number.R
@@ -1,9 +1,9 @@
 sample_number <- function(resource_object) {
     sample_number <- dims(resource_object)[2]
-    if(sample_number > 1) {
-        sample_noun <- "samples"
-    } else {
+    if(sample_number == 1) {
         sample_noun <- "sample"
+    } else {
+        sample_noun <- "samples"
     }
     prettyNum(sample_number, big.mark = ",") %>%
     paste(., sample_noun)
