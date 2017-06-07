4d9679add5b16cff51cca8fc672ce8a87bac7361
schifferl <schifferl@users.noreply.github.com>
2017-06-07 17:20:18 -0400

    corrections to documentation functions

--- data-raw/get_documentation.R
+++ data-raw/get_documentation.R
@@ -9,7 +9,7 @@ get_documentation <- function(resource_name) {
     pmid <- get_experiment_data(resource_object, "pubMedIds") %>% paste0(collapse = "; ")
     technology <- get_technology(resource_object)
     name <- get_name(resource_name)
-    seealso <- strsplit(resource_name, "\\.")[[1]][1]
+    seealso <- strsplit(resource_name, "\\.")[[1]][2]
     data_frame(title, aliases, subsection, publication, authors, affiliations,
                pmid, technology, name, seealso)
 }
