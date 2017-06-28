400f40bc1985d37da3014a09c95f669b3992388d
Levi Waldron <lwaldron@hsph.harvard.edu>
2017-06-28 23:15:54 +0000

    Add LICENCE and CITATION, Edo's updates to vignettes, require bioc-devel versions of ExperimentHub, AnnotationHub, Biobase
    git-svn-id: file:///home/git/bioc-data.hedgehog.fhcrc.org/trunk/experiment/pkgs/curatedMetagenomicData@4193 db2202da-8704-0410-b924-b125b3cd84ef

--- data-raw/documentation_examples.R
+++ data-raw/documentation_examples.R
@@ -1,4 +1,7 @@
 documentation_examples <- function(first_element, documentation_df) {
     documentation_df$aliases[first_element] %>%
+    gsub("genefamilies_relab", "metaphlan_bugs_list", .) %>% {
+        ifelse(grepl("-", .), paste0("`", ., "`"), .)
+    } %>%
     paste0("\n#' @examples ", ., "()\n#'")
 }
--- data-raw/documentation_source.R
+++ data-raw/documentation_source.R
@@ -1,7 +1,8 @@
 documentation_source <- function(first_element, documentation_df) {
     subsection_name <- c("Title", "Author", "Lab", "PMID")
     subsection_text <- documentation_df[first_element, 4:7]
-    bpmapply(subsection_str, subsection_name, subsection_text) %>%
+    ifelse(nchar(subsection_text) > 1, subsection_text, "NA") %>%
+    bpmapply(subsection_str, subsection_name, .) %>%
         unlist() %>%
         as.character() %>%
         paste0(., collapse = "")
