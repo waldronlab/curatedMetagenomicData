2c9e46e4e565c3cf3ec5d8c88915cf519f402318
Lucas Schiffer <schiffer.lucas@gmail.com>
2017-08-14 16:11:22 +0000

    DESCRIPTION changes, into message, version bump
    git-svn-id: file:///home/git/bioc-data.hedgehog.fhcrc.org/branches/RELEASE_3_5/experiment/pkgs/curatedMetagenomicData@4225 db2202da-8704-0410-b924-b125b3cd84ef

--- R/zzz.R
+++ R/zzz.R
@@ -2,12 +2,17 @@
 #' @importFrom AnnotationHub query
 #' @importFrom ExperimentHub ExperimentHub
 #' @importFrom utils globalVariables
+#' @importFrom methods is
+#' @importFrom Biobase isUnique
 #' @keywords internal
 .onLoad <- function(libname, pkgname) {
     titles <- read.csv(system.file("extdata", "metadata.csv",
                                    package="curatedMetagenomicData"),
                        stringsAsFactors=FALSE)$Title
-    rda <- gsub(".rda", "", titles, fixed=TRUE)
+    latest_version <- max(gsub("([^0-9])\\w+", "", titles))
+    versioned_titles <- titles[startsWith(titles, latest_version)]
+    rda <- gsub(".rda", "", versioned_titles, fixed=TRUE)
+    rda <- gsub(paste0(latest_version, "\\."), "", versioned_titles)
     if (!length(rda))
         stop("no .rda objects found in metadata")
 
@@ -15,11 +20,15 @@
     ns <- asNamespace(pkgname)
     sapply(rda,
            function(xx) {
-               func = function(metadata = FALSE) {
+               func = function(cmdversion = latest_version, metadata = FALSE) {
+                   cmdversion <- as.integer(cmdversion)
+                   if(length(cmdversion) > 1 | !.cmdIsValidVersion(cmdversion))
+                       stop("Must provide a single valid version number, see
+                            cmdValidVersions().")
                    if (!isNamespaceLoaded("ExperimentHub"))
                        attachNamespace("ExperimentHub")
                    eh <- query(ExperimentHub(), "curatedMetagenomicData")
-                   ehid <- names(query(eh, xx))
+                   ehid <- names(query(eh, paste0(cmdversion, ".", xx)))
                    if (!length(ehid))
                        stop(paste0("resource ", xx,
                                    " not found in ExperimentHub"))
@@ -32,4 +41,14 @@
            })
 
     globalVariables(".")
+
+    readLines("./inst/extdata/curatedMetagenomicData.txt") %>%
+        paste0(collapse = "\n") %>%
+        message()
+
+    message("Consider using the development version of curatedMetagenomicData,",
+            "\n",
+            "as the database has expanded considerably since the last release.",
+            "\n", "See tinyurl.com/datasets-included for further information.")
 }
+
