7ec508361fc70d2c466e329b6e2025a801d1956a
schifferl <schifferl@users.noreply.github.com>
2017-08-14 11:03:34 -0400

    correction to metadata description string creation

--- data-raw/description_str.R
+++ data-raw/description_str.R
@@ -2,27 +2,27 @@ description_str <- function(description_chr) {
     description_suffix <- paste("data from the", description_chr[2],
                                 "dataset specific to the", description_chr[4],
                                 "body site.")
-    if(description_chr[3] == "genefamilies_relab") {
+    if(description_chr[2] == "genefamilies_relab") {
         description_str <- paste("Relabeled genefamilies", description_suffix)
         return(description_str)
     }
-    if(description_chr[3] == "marker_abundance") {
+    if(description_chr[2] == "marker_abundance") {
         description_str <- paste("Marker abundance", description_suffix)
         return(description_str)
     }
-    if(description_chr[3] == "marker_presence") {
+    if(description_chr[2] == "marker_presence") {
         description_str <- paste("Marker presence", description_suffix)
         return(description_str)
     }
-    if(description_chr[3] == "metaphlan_bugs_list") {
+    if(description_chr[2] == "metaphlan_bugs_list") {
         description_str <- paste("Taxonomic abundance", description_suffix)
         return(description_str)
     }
-    if(description_chr[3] == "pathabundance_relab") {
+    if(description_chr[2] == "pathabundance_relab") {
         description_str <- paste("Relabeled pathabundance", description_suffix)
         return(description_str)
     }
-    if(description_chr[3] == "pathcoverage") {
+    if(description_chr[2] == "pathcoverage") {
         description_str <- paste("Pathcoverage", description_suffix)
         return(description_str)
     }
