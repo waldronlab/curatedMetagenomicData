0f96d77c157bd633e5ff936051a2d42e0d4e3781
Sean Davis <seandavi@gmail.com>
2017-09-15 20:57:38 +0000

    Full stack docker container added

---
+++ inst/docker/curatedMetagenomics/README.md
@@ -0,0 +1,14 @@
+
+## Usage
+
+```
+#docker run -v /PATH_TO_WHERE_YOU_WANT_OUTPUT:/RUN_PATH_IN_CONTAINER -ti seandavi/curatedmetagenomics SAMPLENAME SRA_ACCESSION /RUN_PATH_IN_CONTAINER
+# a runnable example
+docker run -v /tmp:/tmp -ti seandavi/curatedmetagenomics /bin/bash curatedMetagenomicData_pipeline.sh SAMPLE ERR262957 /tmp
+```
+
+## Build
+
+```sh
+docker build --tag seandavi/curatedmetagenomics .
+```
---
+++ inst/docker/curatedMetagenomics/curatedMetagenomicData_pipeline.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+### usage: bash curatedMetagenomicData_pipeline.sh sample_name "SRRxxyxyxx;SRRyyxxyyx"
+
+### before running this script, be sure that these tools are in your path
+# fastq-dump
+# humann2
+# metaphlan2
+# python
+
+### before running this script, set these paths and variables
+pm=/opt/metaphlan2/biobakery-metaphlan2/metaphlan2.py #metaphlan2 path (like /tools/metaphlan2/bin/metaphlan2.py)
+pc=/dbs/humann2/chocophlan # chocophlan database (nucleotide-database for humann2, like /databases/chocophlan
+pp=/dbs/humann2/uniref # uniref database (protein-database for humann2, like /databases/uniref)
+pmdb=/dbs/db_v20/mpa_v20_m200.pkl #metaphlan2 database (like /tools/metaphlan2/db_v20/mpa_v20_m200.pkl)
+ncores=8 #number of cores
+
+sample=$1
+runs=$2
+workdir=$3
+
+cd $workdir
+
+# while [ "$runs" ] ; do
+# 	iter=${runs%%;*}
+#         shortyone=$(echo "$iter" | cut -c1-3)
+#         shortytwo=$(echo "$iter" | cut -c1-6)
+# 	echo 'Starting downloading run '${iter}
+#         ${pa}bin/ascp -T -i ${pa}etc/asperaweb_id_dsa.openssh anonftp@ftp.ncbi.nlm.nih.gov:/sra/sra-instant/reads/ByRun/sra/${shortyone}/${shortytwo}/${iter}/${iter}.sra ${sample}/reads/
+# 	echo 'Dumping run '${iter}
+#         fastq-dump --split-files ${sample}/reads/${iter}.sra --outdir ${sample}/reads/
+#         [ "$runs" = "$iter" ] && \
+#         runs='' || \
+#         runs="${runs#*;}"
+# 	echo 'Finished downloading of run '${iter}
+# done
+fastq-dump --outdir reads $2
+echo 'Downloaded.'
+echo 'Concatenating runs...'
+cat reads/*.fastq > reads/${sample}.fastq
+
+mkdir -p humann2
+echo 'Running humann2'
+humann2 --input reads/${sample}.fastq --output humann2 --nucleotide-database ${pc} --protein-database ${pp} --threads=${ncores}
+echo 'renorm_table runs'
+humann2_renorm_table --input humann2/${sample}_genefamilies.tsv --output humann2/${sample}_genefamilies_relab.tsv --units relab
+humann2_renorm_table --input humann2/${sample}_pathabundance.tsv --output humann2/${sample}_pathabundance_relab.tsv --units relab
+echo 'run_markers2.py'
+# NOTE: using absolute path here!!!
+python /root/run_markers2.py --input_dir humann2/${sample}_humann2_temp/ --bt2_ext _metaphlan_bowtie2.txt --metaphlan_path ${pm} --metaphlan_db ${pmdb} --output_dir humann2 --nprocs ${ncores}
+
+mkdir genefamilies; mv humann2/${sample}_genefamilies.tsv genefamilies/${sample}.tsv;
+mkdir genefamilies_relab; mv humann2/${sample}_genefamilies_relab.tsv genefamilies_relab/${sample}.tsv;
+mkdir marker_abundance; mv humann2/${sample}.marker_ab_table marker_abundance/${sample}.tsv;
+mkdir marker_presence; mv humann2/${sample}.marker_pres_table marker_presence/${sample}.tsv;
+mkdir metaphlan_bugs_list; mv humann2/${sample}_humann2_temp/${sample}_metaphlan_bugs_list.tsv metaphlan_bugs_list/${sample}.tsv;
+mkdir pathabundance; mv humann2/${sample}_pathabundance.tsv pathabundance/${sample}.tsv;
+mkdir pathabundance_relab; mv humann2/${sample}_pathabundance_relab.tsv pathabundance_relab/${sample}.tsv;
+mkdir pathcoverage; mv humann2/${sample}_pathcoverage.tsv pathcoverage/${sample}.tsv;
+mkdir humann2_temp; mv humann2/${sample}_humann2_temp/ humann2_temp/ #comment this line if you don't want to keep humann2 temporary files
+
+#rm -r reads humann2
