6d55acc92726dbd11f07c7a1fc1fc219e031d462
Levi Waldron <lwaldron.research@gmail.com>
2018-10-24 23:22:10 -0400

    Unit test compares datasets in combined_metadata, inst/extdata/metadata.csv, and curatedMetagenomicData().

--- tests/testthat/test-curatedMetagenomicData.R
+++ tests/testthat/test-curatedMetagenomicData.R
@@ -1,30 +1,58 @@
 context("curatedMetagenomicData")
 
 test_that("curatedMetagenomicData works", {
+    ## studies from curatedMetagenomicData()
     allds <- curatedMetagenomicData("*", dryrun = TRUE)
-    allstudies1 <- sub("\\..+", "", allds) %>%
+    allstudies.cmd <- sub("\\..+", "", allds) %>%
         unique %>%
         sort
+    ## studies from combined_metadata
     data("combined_metadata")
-    allstudies2 <- unique(combined_metadata$dataset_name) %>% sort
-    munged_combined_metadata <- grep("^[A-Za-z]+_[A-Za-z]", allstudies2, value = TRUE)
-    expect_false(length(munged_combined_metadata) > 0,
-                  info = paste("Munged datasets in combined_metadata$dataset_name: ",
-                               munged_combined_metadata, collapse = " "))
-    extrastudies1 <- paste(allstudies1[!allstudies1 %in% allstudies2], collapse=" ")
-    extrastudies2 <- paste(allstudies2[!allstudies2 %in% allstudies1], collapse=" ")
-    expect_equal(allstudies1, allstudies2,
+    allstudies.combined_metadata <- unique(combined_metadata$dataset_name) %>% sort
+    ## studies from inst/extdata/metadata.csv
+    filename <- system.file("extdata/metadata.csv", package="curatedMetagenomicData")
+    allstudies.metadata.csv <- read.csv(filename, as.is = TRUE)$Title %>%
+        grep("^[0-9]", ., invert = TRUE, value = TRUE) %>%
+        sub("\\..+", "", .) %>%
+        unique %>%
+        sort
+    ## compare curatedMetagenomicData() to combined_metadata
+    extrastudies1 <- paste(setdiff(allstudies.cmd, allstudies.combined_metadata), collapse=" ")
+    extrastudies2 <- paste(setdiff(allstudies.combined_metadata, allstudies.cmd), collapse=" ")
+    expect_identical(allstudies.cmd, allstudies.combined_metadata,
                  info = paste("curatedMetagenomicData() and combined_metadata return the same datasets\n",
                               "in curatedMetagenomicData() but not combined_metadata: ", extrastudies1,
                               "\n in combined_metadata but not curatedMetagenomicData(): ", extrastudies2,
                               collapse=""))
+    ## compare inst/extdata/metadata.csv to combined_metadata
+    extrastudies3 <- paste(setdiff(allstudies.metadata.csv, allstudies.combined_metadata), collapse=" ")
+    extrastudies4 <- paste(setdiff(allstudies.combined_metadata, allstudies.metadata.csv), collapse=" ")
+    expect_identical(allstudies.metadata.csv, allstudies.combined_metadata,
+                 info = paste("inst/extdata/metadata.csv and combined_metadata have the same datasets",
+                              "\n in inst/extdata/metadata.csv but not combined_metadata: ", extrastudies3,
+                              "\n in combined_metadata but not inst/extdata/metadata.csv: ", extrastudies4,
+                              collapse=""))
+    ## check for munged dataset names
+    checkMunging <- function(x, msg){
+        munged <- grep("^[A-Za-z]+_[A-Za-z]", x, value = TRUE)
+        expect_false(length(munged) > 0,
+                     info = paste("Munged datasets in", msg, ":", paste(munged, collapse=" "), collapse = " "))
+    }
+    checkMunging(allstudies.combined_metadata, "combined_metadata")
+    checkMunging(allstudies.cmd, "curatedMetagenomicData()")
+    checkMunging(allstudies.metadata.csv, "inst/extdata/metadata.csv")
+    ##
     datatypes <- c("pathcoverage", "pathabundance_relab", "metaphlan_bugs_list",
                    "marker_presence", "marker_abundance", "genefamilies_relab") %>%
         paste(., collapse="|")
     allstudies3 <- sub(datatypes, "", allds) %>% unique
-    expect_equal(length(allds), length(allstudies3) * 6) #6 data products per study
-    expect_true(all(grepl(datatypes, allds))) #all datasets are of known data type
-    ##
+    expect_equal(length(allds), length(allstudies3) * 6,
+                 info="6 data products per study")
+    expect_true(all(grepl(datatypes, allds)),
+                info="all datasets are of known data type")
+})
+
+test_that("BritoIL_2016 and Castro-NallarE", {
     oral <- c("BritoIL_2016.metaphlan_bugs_list.oralcavity",
               "Castro-NallarE_2015.metaphlan_bugs_list.oralcavity")
     esl <- curatedMetagenomicData(oral, dryrun = FALSE, counts=TRUE)
