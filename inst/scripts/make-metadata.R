265f153b85044d038204ee6dc5241f3d1319aa9e
schifferl <schifferl@users.noreply.github.com>
2021-04-09 21:05:08 +0000

    update metadata files

--- inst/scripts/make-metadata.R
+++ inst/scripts/make-metadata.R
@@ -1,5 +1,3 @@
-library(magrittr)
-
-base::dir(path = "inst/extdata", pattern = "[0-9]+", full.names = TRUE) %>%
-    purrr::map_dfr(~ readr::read_csv(.x, col_types = "ccccccccclcccccc")) %>%
+base::dir(path = "inst/extdata", pattern = "[0-9]+", full.names = TRUE) |>
+    purrr::map_dfr(~ readr::read_csv(.x, col_types = "ccccccccclcccccc")) |>
     readr::write_csv("inst/extdata/metadata.csv")
