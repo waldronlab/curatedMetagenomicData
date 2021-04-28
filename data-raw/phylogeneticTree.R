c563e611264007a164a760db479123b0fdb6c584
schifferl <schifferl@users.noreply.github.com>
2021-04-28 01:54:52 +0000

    add phylogeneticTree object

--- data-raw/phylogeneticTree.R
+++
@@ -1,9 +0,0 @@
-
-metaphlan_tree_file <- base::tempfile()
-metaphlan_tree_url <- "https://raw.githubusercontent.com/biobakery/MetaPhlAn/master/metaphlan/utils/mpa_v30_CHOCOPhlAn_201901_species_tree.nwk"
-utils::download.file(url = metaphlan_tree_url, destfile = metaphlan_tree_file)
-phylogeneticTree <- ape::read.tree(metaphlan_tree_file)
-phylogeneticTree$tip.label <- base::sub("\\|GCF_[0-9]+$", "", phylogeneticTree$tip.label)
-phylogeneticTree$tip.label <- base::gsub(".+\\|", "", phylogeneticTree$tip.label)
-usethis::use_data(phylogeneticTree)
-
--- data-raw/sysdata.R
+++ data-raw/sysdata.R
@@ -1,20 +1,18 @@
 library(magrittr)
 
-installed_packages <-
-    utils::installed.packages() %>%
-    base::rownames()
+phylogenetic_tree_file <-
+    base::as.character("https://raw.githubusercontent.com/biobakery/MetaPhlAn/master/metaphlan/utils/mpa_v30_CHOCOPhlAn_201901_species_tree.nwk")
 
-if (!("BiocManager" %in% installed_packages)) {
-    utils::install.packages("BiocManager")
-}
+phylogenetic_tree_path <-
+    base::tempfile()
 
-if (!("readr" %in% installed_packages)) {
-    BiocManager::install("readr")
-}
+utils::download.file(url = phylogenetic_tree_file, destfile = phylogenetic_tree_path)
 
-if (!("usethis" %in% installed_packages)) {
-    BiocManager::install("usethis")
-}
+phylogeneticTree <-
+    ape::read.tree(phylogenetic_tree_path)
+
+phylogeneticTree[["tip.label"]] <-
+    stringr::str_remove_all(phylogeneticTree[["tip.label"]], "GCA_[0-9]+\\|")
 
 col_types <-
     readr::cols(
@@ -23,12 +21,12 @@ col_types <-
     )
 
 into_cols <-
-    base::c("dateAdded", "dataset_name", "dataType")
+    base::c("dateAdded", "studyName", "dataType")
 
-Title <-
+title <-
     readr::read_csv("inst/extdata/metadata.csv", col_types = col_types) %>%
     tidyr::separate(Title, into_cols, sep = "\\.", remove = FALSE) %>%
-    dplyr::arrange(dataset_name, dateAdded, dataType) %>%
+    dplyr::arrange(studyName, dateAdded, dataType) %>%
     dplyr::pull(.data[["Title"]])
 
-usethis::use_data(Title, internal = TRUE, overwrite = TRUE)
+usethis::use_data(phylogeneticTree, title, internal = TRUE, overwrite = TRUE)
