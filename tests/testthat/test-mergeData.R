4481491633efa364664c8bbf77d2e99038077450
Samuel Gamboa <Samuel.Gamboa.Tuz@gmail.com>
2021-05-07 09:45:33 -0500

    updated test-mergeData.R

--- tests/testthat/test-mergeData.R
+++ tests/testthat/test-mergeData.R
@@ -1,15 +1,43 @@
 test_that("you can't merge different data types.", {
+
     mergeList <- curatedMetagenomicData("AsnicarF_2017.marker_", dryrun = FALSE)
     expect_error(mergeData(mergeList))
+
 })
 
 test_that("merge results are correct.", {
+
     AsnicarF_2017 <- curatedMetagenomicData("AsnicarF_2017.marker_presence", dryrun = FALSE)[[1]]
     AsnicarF_2021 <- curatedMetagenomicData("AsnicarF_2021.marker_presence", dryrun = FALSE)[[1]]
+
     mergeList <- curatedMetagenomicData("AsnicarF_.+marker_presence", dryrun = FALSE)
+
     testData <- mergeData(mergeList)
     testData2017 <- testData[rownames(AsnicarF_2017), colnames(AsnicarF_2017)]
     testData2021 <- testData[rownames(AsnicarF_2021), colnames(AsnicarF_2021)]
-    expect_equal(AsnicarF_2017, testData2017)
-    expect_equal(AsnicarF_2021, testData2021)
+
+    colData(testData2017) <- colData(testData2017)[, colnames(colData(AsnicarF_2017))]
+    colData(testData2021) <- colData(testData2021)[, colnames(colData(AsnicarF_2021))]
+
+    expect_equal(assay(AsnicarF_2017), assay(testData2017))
+    expect_equal(colData(AsnicarF_2017), colData(testData2017))
+    expect_equal(rowData(AsnicarF_2017), rowData(testData2017))
+
+    expect_equal(assay(AsnicarF_2021), assay(testData2021))
+    expect_equal(colData(AsnicarF_2021), colData(testData2021))
+    expect_equal(rowData(AsnicarF_2021), rowData(testData2021))
+
+})
+
+test_that("returnType is correct.", {
+
+    marker_presence <- curatedMetagenomicData("AsnicarF_.+marker_presence", dryrun = FALSE)
+    test_marker_presence <- mergeData(marker_presence)
+
+    relative_abundance <- curatedMetagenomicData("AsnicarF_.+relative_abundance", dryrun = FALSE)
+    test_relative_abundance <- mergeData(relative_abundance)
+
+    expect_s4_class(marker_presence, "SummarizedExperiment")
+    expect_s4_class(relative_abundance, "TreeSummarizedExperiment")
+
 })
