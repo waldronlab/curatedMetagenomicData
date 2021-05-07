68c96bb970796dddc29c664b4f81f2419b3c4180
Samuel Gamboa <Samuel.Gamboa.Tuz@gmail.com>
2021-05-07 08:41:36 -0500

    returnSamples test

--- tests/testthat/test-returnSamples.R
+++ tests/testthat/test-returnSamples.R
@@ -1,11 +1,10 @@
-test_that("returnSamples returns the same metadata passed to it", {
-    filteredMetadata <-
-        dplyr::filter(sampleMetadata, age == 40, smoker == "yes", gender == "male")
-
-    result <-
-        returnSamples(filteredMetadata, "marker_presence")
-
-    expect_identical(base::colnames(result), filteredMetadata$sample_id)
-
-    expect_identical(result$age, filteredMetadata$age)
+test_that("age is the same after merge", {
+    fm <- dplyr::filter(combined_metadata, age == 40, smoker == "yes", gender == "male")
+    e <- returnSamples(fm, "marker_presence")
+    sample1_fm <- dplyr::filter(fm, subject_id == "Travel006")$age
+    sample2_fm <- dplyr::filter(fm, subject_id == "wHAXPI043593-9")$age
+    sample1_cd <- colData(e)[colData(e)$subject_id == "Travel006", ]$age
+    sample2_cd <- colData(e)[colData(e)$subject_id == "wHAXPI043593-9", ]$age
+    expect_equal(sample1_fm, sample1_cd)
+    expect_equal(sample2_fm, sample2_cd)
 })
