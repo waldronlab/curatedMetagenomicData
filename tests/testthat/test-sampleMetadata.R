a5d084cd323e48696a61c33461fed85f55e5b45c
Samuel Gamboa <Samuel.Gamboa.Tuz@gmail.com>
2021-05-10 01:49:56 -0500

    update test-sampleMetadata

--- tests/testthat/test-sampleMetadata.R
+++ tests/testthat/test-sampleMetadata.R
@@ -1,3 +1,35 @@
-test_that("multiplication works", {
-    expect_equal(2 * 2, 4)
+data("sampleMetadata", "combined_metadata")
+
+test_that("all studies from cMD (metadata file) are in sampleMetadata", {
+
+    expectedStudyNames <- system.file("extdata/metadata.csv", package = "curatedMetagenomicData") %>%
+        read.csv() %>%
+        .[["Title"]] %>%
+        sub("^.+\\.(.+)\\..+$", "\\1", .) %>%
+        unique() %>%
+        sort()
+
+    testStudyNames <- sampleMetadata[["study_name"]] %>%
+        unique() %>%
+        sort()
+
+    expect_identical(testStudyNames, expectedStudyNames)
+
+})
+
+test_that("all required columns are present in sampleMetadata", {
+
+    requiredColumns <- system.file("extdata/template.csv", package = "curatedMetagenomicDataCuration") %>%
+        read.csv() %>%
+        subset(requiredness == "required") %>%
+        .[["col.name"]]
+
+    expect_true(all(requiredColumns %in% colnames(sampleMetadata)))
+
+})
+
+test_that("combined_metadata and sampleMetadata are the same", {
+
+    expect_identical(combined_metadata, sampleMetadata)
+
 })
