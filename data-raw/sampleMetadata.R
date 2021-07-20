0456ad9e1120b246005b7cdab7a1b91c266dda99
schifferl <schifferl@users.noreply.github.com>
2021-07-20 19:15:29 +0000

    update package files

--- data-raw/sampleMetadata.R
+++ data-raw/sampleMetadata.R
@@ -1,5 +1,3 @@
-library(magrittr)
-
 BiocManager::install("waldronlab/curatedMetagenomicDataCuration")
 
 col_types <-
@@ -14,35 +12,35 @@ col_types <-
     )
 
 glue_str <-
-    base::system.file("extdata", "template.csv", package = "curatedMetagenomicDataCuration") %>%
-    readr::read_csv(col_types = col_types) %>%
-    glue::glue_data('{col.name} = readr::col_{col.class}()') %>%
+    base::system.file("extdata", "template.csv", package = "curatedMetagenomicDataCuration") |>
+    readr::read_csv(col_types = col_types) |>
+    glue::glue_data('{col.name} = readr::col_{col.class}()') |>
     glue::glue_collapse(sep = ", ")
 
 cols_str <-
     glue::glue('readr::cols({glue_str})')
 
 col_types <-
-    base::parse(text = cols_str) %>%
+    base::parse(text = cols_str) |>
     base::eval()
 
 base::load("R/sysdata.rda")
 
 valid_name <-
-    stringr::str_extract(title, "[A-Z].+") %>%
-    stringr::str_remove("\\..+") %>%
+    stringr::str_extract(title, "[A-Z].+") |>
+    stringr::str_remove("\\..+") |>
     base::unique()
 
 sampleMetadata <-
-    base::system.file("curated", package = "curatedMetagenomicDataCuration") %>%
-    base::dir(pattern = "tsv", full.names = TRUE, recursive = TRUE) %>%
-    purrr::set_names(nm = ~ base::basename(.x)) %>%
-    purrr::set_names(nm = ~ stringr::str_remove(.x, "_metadata.tsv")) %>%
-    purrr::map(~ readr::read_tsv(.x, col_types = col_types)) %>%
-    purrr::imap(~ dplyr::mutate(.x, study_name = .y, .before = "sample_id")) %>%
-    purrr::reduce(dplyr::bind_rows) %>%
-    dplyr::filter(study_name %in% valid_name) %>%
-    dplyr::select(where(~ !base::all(base::is.na(.x)))) %>%
+    base::system.file("curated", package = "curatedMetagenomicDataCuration") |>
+    base::dir(pattern = "tsv", full.names = TRUE, recursive = TRUE) |>
+    purrr::set_names(nm = ~ base::basename(.x)) |>
+    purrr::set_names(nm = ~ stringr::str_remove(.x, "_metadata.tsv")) |>
+    purrr::map(~ readr::read_tsv(.x, col_types = col_types)) |>
+    purrr::imap(~ dplyr::mutate(.x, study_name = .y, .before = "sample_id")) |>
+    purrr::reduce(dplyr::bind_rows) |>
+    dplyr::filter(study_name %in% valid_name) |>
+    dplyr::select(where(~ !base::all(base::is.na(.x)))) |>
     base::as.data.frame()
 
 usethis::use_data(sampleMetadata, overwrite = TRUE)
