3e936c03b3bdff30faaf08471a0dc9e6e4e4b6e8
Tuomas Borman <tvborm@utu.fi>
2025-03-20 19:14:40 +0200

    Fix NCBI-style short names

--- R/curatedMetagenomicData.R
+++ R/curatedMetagenomicData.R
@@ -79,7 +79,7 @@
 #' @importFrom S4Vectors SimpleList
 #' @importFrom TreeSummarizedExperiment TreeSummarizedExperiment
 #' @importFrom SummarizedExperiment rowData<-
-#' @importFrom mia taxonomyRankEmpty getTaxonomyLabels
+#' @importFrom mia taxonomyRankEmpty getTaxonomyLabels taxonomyRanks
 #' @importFrom TreeSummarizedExperiment rownames<-
 #' @importFrom SummarizedExperiment rowData
 #' @importFrom SummarizedExperiment assay<-
@@ -255,7 +255,16 @@ curatedMetagenomicData <- function(pattern, dryrun = TRUE, counts = FALSE, rowna
                 }
                 # Get shorter labels
                 rownames(tree_summarized_experiment) <-
-                    getTaxonomyLabels(tree_summarized_experiment)
+                    getTaxonomyLabels(tree_summarized_experiment, make.unique = FALSE)
+                # If user wants NCBI-style rownames, we remove possible rank
+                # prefix. The labels might have the prefix, if taxon was
+                # identified only in genus level while others had species level.
+                # Usually rank prefix is useful information, but in NCBI-style
+                # taxa are just unique IDs, so we do not need the prefix.
+                if( rownames == "NCBI" ){
+                    pattern <- paste0(paste0(taxonomyRanks(tree_summarized_experiment), ":"), collapse = "|")
+                    rownames(tree_summarized_experiment) <- gsub(pattern, "", rownames(tree_summarized_experiment)) |> as.integer()
+                }
             }
 
             resource_list[[i]] <-
